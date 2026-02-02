3a0bb8f562214330d1ba5f17367bd455e343b007
mertalev <101130780+mertalev@users.noreply.github.com>
2026-02-02 13:11:45 -0500

    use shared client in dart
    fix android

--- mobile/lib/services/api.service.dart
+++ mobile/lib/services/api.service.dart
@@ -3,12 +3,11 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:http/http.dart';
 import 'package:immich_mobile/domain/models/store.model.dart';
 import 'package:immich_mobile/entities/store.entity.dart';
+import 'package:immich_mobile/infrastructure/repositories/network.repository.dart';
 import 'package:immich_mobile/utils/debug_print.dart';
 import 'package:immich_mobile/utils/url_helper.dart';
-import 'package:immich_mobile/utils/user_agent.dart';
 import 'package:logging/logging.dart';
 import 'package:openapi/api.dart';
 
@@ -50,7 +49,7 @@ class ApiService implements Authentication {
 
   setEndpoint(String endpoint) {
     _apiClient = ApiClient(basePath: endpoint, authentication: this);
-    _setUserAgentHeader();
+    _apiClient.client = NetworkRepository.client;
     if (_accessToken != null) {
       setAccessToken(_accessToken!);
     }
@@ -74,11 +73,6 @@ class ApiService implements Authentication {
     viewApi = ViewsApi(_apiClient);
     memoriesApi = MemoriesApi(_apiClient);
     sessionsApi = SessionsApi(_apiClient);
-  }
-
-  Future<void> _setUserAgentHeader() async {
-    final userAgent = await getUserAgentString();
-    _apiClient.addDefaultHeader('User-Agent', userAgent);
   }
 
   Future<String> resolveAndSetEndpoint(String serverUrl) async {
@@ -134,13 +128,11 @@ class ApiService implements Authentication {
   }
 
   Future<String> _getWellKnownEndpoint(String baseUrl) async {
-    final Client client = Client();
-
     try {
       var headers = {"Accept": "application/json"};
       headers.addAll(getRequestHeaders());
 
-      final res = await client
+      final res = await NetworkRepository.client
           .get(Uri.parse("$baseUrl/.well-known/immich"), headers: headers)
           .timeout(const Duration(seconds: 5));
 
